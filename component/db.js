14ceca0da752a7e74667a67b1da78a8b349839ce
yuan8 <yuan.putra@windowslive.com>
2017-05-30 04:22:37 +0700

    db buatan

---
+++ component/db.js
@@ -0,0 +1,28 @@
+var db=[
+	{
+		name:'level 1',
+		title:'living room',
+		ava:'',
+		quest:[
+				{
+					img:'',
+					ans:'chair'
+				},
+				{
+					img:'',
+					ans:'table'
+				}	
+			]
+	},
+	{
+		name:'level 2',
+		title:'kitchen',
+		ava:'',
+		quest:[
+				{
+					img:'',
+					ans:'hook'
+				}	
+			]
+	}
+]
