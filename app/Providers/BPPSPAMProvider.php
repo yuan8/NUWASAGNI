0b6ed5e9d03073caa3075bb7bf3501a835a00ba0
yuan8 <bhalayuan@gmail.com>
2020-07-02 21:48:11 +0700

    ok

--- app/Providers/BPPSPAMProvider.php
+++ app/Providers/BPPSPAMProvider.php
@@ -205,30 +205,30 @@ class BPPSPAMProvider extends ServiceProvider
                     $table->tinyInterger('kode_buku')->default(0);
                     $table->integer('kode_hal')->default(0);
                     $table->tinyInterger('tahun')->default(0);
-                    $table->double('roe')->default(0),
-                    $table->double('ratio_operasi')->default(0),
-                    $table->double('ratio_kas')->default(0),
-                    $table->double('efektivitas_penagihan')->default(0),
-                    $table->double('solvabilitas')->default(0),
-                    $table->double('kinerja_keuangan')->default(0),
-                    $table->double('cakupan_pelayanan')->default(0),
-                    $table->double('pertumbuhan_pelanggan')->default(0),
-                    $table->double('tingkat_penyelesaian_pengaduan')->default(0),
-                    $table->double('kualitas_air_pelanggan')->default(0),
-                    $table->double('konsumsi_air_domestik')->default(0),
-                    $table->double('kinerja_pelayanan')->default(0),
-                    $table->double('efesiensi_produksi')->default(0),
-                    $table->double('tingkat_kehilangan_air')->default(0),
-                    $table->double('jam_operasi_layanan')->default(0),
-                    $table->double('tekanan_sambungan_pelanggan')->default(0),
-                    $table->double('penggantian_meter_air')->default(0),
-                    $table->double('kinerja_operasi')->default(0),
-                    $table->double('ratio_jumlah_pegawai')->default(0),
-                    $table->double('ratio_diklat_pegawai')->default(0),
-                    $table->double('biaya_diklat_thd_pegawai')->default(0),
-                    $table->double('kinerja_sdm')->default(0),
-                    $table->double('kinerja_total')->default(0),
-                    $table->double('kategori')->default(0),
+                    $table->double('roe',25,3)->default(0);
+                    $table->double('ratio_operasi',25,3)->default(0);
+                    $table->double('ratio_kas',25,3)->default(0);
+                    $table->double('efektivitas_penagihan',25,3)->default(0);
+                    $table->double('solvabilitas',25,3)->default(0);
+                    $table->double('kinerja_keuangan',25,3)->default(0);
+                    $table->double('cakupan_pelayanan',25,3)->default(0);
+                    $table->double('pertumbuhan_pelanggan',25,3)->default(0);
+                    $table->double('tingkat_penyelesaian_pengaduan',25,3)->default(0);
+                    $table->double('kualitas_air_pelanggan',25,3)->default(0);
+                    $table->double('konsumsi_air_domestik',25,3)->default(0);
+                    $table->double('kinerja_pelayanan',25,3)->default(0);
+                    $table->double('efesiensi_produksi',25,3)->default(0);
+                    $table->double('tingkat_kehilangan_air',25,3)->default(0);
+                    $table->double('jam_operasi_layanan',25,3)->default(0);
+                    $table->double('tekanan_sambungan_pelanggan',25,3)->default(0);
+                    $table->double('penggantian_meter_air',25,3)->default(0);
+                    $table->double('kinerja_operasi',25,3)->default(0);
+                    $table->double('ratio_jumlah_pegawai',25,3)->default(0);
+                    $table->double('ratio_diklat_pegawai',25,3)->default(0);
+                    $table->double('biaya_diklat_thd_pegawai',25,3)->default(0);
+                    $table->double('kinerja_sdm',25,3)->default(0);
+                    $table->double('kinerja_total',25,3)->default(0);
+                    $table->string('kategori',60)->default(0);
                     $table->mediumText('keterangan')->nullable();
 
                     $table->unique(['id_pdam','tahun']);
